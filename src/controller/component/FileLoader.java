c6047a834bfe7858713d5ea421e894bb0e0577b7
Vichhika <vichhikapann@gmail.com>
2021-04-10 22:07:42 +0700

    load txt or csv file extension

--- src/controller/component/FileLoader.java
+++ src/controller/component/FileLoader.java
@@ -1,11 +1,29 @@
 package controller.component;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.io.File;
 
 public class FileLoader extends JFileChooser {
 
-    public
+    public FileLoader(){
+        this.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                if(f.isDirectory()){
+                    return true;
+                } else {
+                    return f.getName().endsWith(".txt") || f.getName().endsWith(".csv");
+                }
+            }
+
+            @Override
+            public String getDescription() {
+                return "text file (*.txt) or csv file (*.csv)";
+            }
+        });
+        this.setAcceptAllFileFilterUsed(false);
+    }
 
     public void loadFile(){
         int DIALOG_CONDITION = this.showDialog(null,"Load");
